0de5ec75544dc6815c1b6af66e96bc295a0bcee2
Joe Rodriguez <joerod@gmail.com>
2015-03-03 15:13:42 -0500

    Update bash_pipe.py

--- bash_pipe.py
+++ bash_pipe.py
@@ -1,16 +1,18 @@
-__author__ = 'joerod'
+author__ = 'joerod'
 
-import subprocess
+import subprocess,os
 
-openfiles = subprocess.Popen(['lsof'],
+path = '/tmp/files'
+for file in ([os.path.join(path,fn)for fn in next(os.walk(path))[2]]):
+    openfiles = subprocess.Popen(['/usr/sbin/lsof'],
                              stdout=subprocess.PIPE)
-grep = subprocess.Popen(['grep', 'read.txt'],
+    grep = subprocess.Popen(['grep', file],
                         stdin=openfiles.stdout,
                         stdout=subprocess.PIPE
                         )
-openfiles.stdout.close()
-output =  grep.communicate()[0]
+    openfiles.stdout.close()
+    output =  (grep.communicate()[0]).decode('utf-8')
 
-if output != '':
-    print 'There is a file open'
-    print output
+    if (output != ''):
+        print ('There is a file open')
+        print (output)
