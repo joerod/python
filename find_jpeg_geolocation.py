e3a079d0aa613fd12feee35159129b7a2d073d62
Joe Rodriguez <joerod@gmail.com>
2015-10-17 22:38:54 -0400

    Update find_jpeg_geolocation.py

--- find_jpeg_geolocation.py
+++ find_jpeg_geolocation.py
@@ -1,21 +1,22 @@
-#this script searches a directory for jpgs then extracts the geolocation. 
+"""
+Created on Mon Oct 12 10:55:36 2015
 
-import OS
-from pexif import JpegFile
+@author: joerod
+"""
 
-workingdir = '/Users/joerod/Pictures'
+import pexif, os, json, urllib2
+#from pprint import pprint
+
+workingdir = "/Users/joerod/Pictures"
+key = ''
 
 for file in os.listdir(workingdir):
     if file.endswith(".JPG"):
-        try:
-            print(pexif.JpegFile.fromFile(workingdir + "/" + file).get_geo())
-        except IOError:
-            type, value, traceback = sys.exc_info()
-            print >> sys.stderr, "Error opening file:", value
-        except JpegFile.NoSection:
-            type, value, traceback = sys.exc_info()
-            print >> sys.stderr, "Error get GPS info:", value
-       except JpegFile.InvalidFile:
-           type, value, traceback = sys.exc_info()
-           print >> sys.stderr, "Error opening file:", value
+        lat_log = (pexif.JpegFile.fromFile(workingdir + "/" + file)).get_geo()
+        lat_log = str(lat_log).replace('(','').replace(')','')
+        url = "https://maps.googleapis.com/maps/api/geocode/json?latlng={0}&key={1}".format(lat_log,key).replace(" ","")
+        data = json.load(urllib2.urlopen(url))
+        #print urlobj
+        print data['results'][0]['formatted_address']
     
+  
