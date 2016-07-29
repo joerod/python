dfe9a62eb9aa13f913724e0cded0644fb35b2319
Joe Rodriguez <joerod@gmail.com>
2016-07-28 21:35:28 -0400

    Create reboot_pi_no_network.py

---
+++ raspberry_pi/reboot_pi_no_network.py
@@ -0,0 +1,17 @@
+#This will reboot my Raspberry Pi if it does not detect a network connection
+import os,datetime
+
+logfile = '/home/pi/Documents/Logs/logs.log'
+
+def check_ping(host):
+    response = os.system("ping -c 1 " + host)
+    # and then check the response...
+    if response == 0:
+        with open(logfile,"a") as myfile:
+            myfile.write("{0} - Network Active".format(str(datetime.datetime.now())))
+    else:
+        with open(logfile,"a") as myfile:
+            myfile.write("{0} - Network Down".format(str(datetime.datetime.now())))
+        os.system("sudo shutdown -r now")
+
+check_ping("192.168.1.1")
