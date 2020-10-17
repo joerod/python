806a41c2fdba21daef6c176401fd2848daee6b45
Joe Rodriguez <joerod@gmail.com>
2020-10-17 16:28:09 -0400

    updated example to use arguments from url in PowerShell script

--- flask_powershell/api.py
+++ flask_powershell/api.py
@@ -3,20 +3,22 @@ import subprocess
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def index():
-    return execute_script("test")
-    return name
-
-#url: http://127.0.0.1:5000/users?username=joerod
-@app.route("/users", methods=['GET'])
-def name():
-    username = request.args.get('username')
-    return username 
+#url: http://127.0.0.1:5000/process?processname=chrome
+@app.route("/process", methods=['GET'])
+def name(script = 'test2'):
+    username = request.args.get('processname') # take arguments from the url and use them in scripts
+    cmd = ["powershell","-ExecutionPolicy", "Bypass", ".\{0}.ps1".format(script),username]
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    out,err = p.communicate()
+    print(out)
+    if(err):
+        raise Exception('Error: ' + str(err))
+    return out
 
 #url: http://127.0.0.1:5000/
-def execute_script(script):
-    cmd = ["powershell","-ExecutionPolicy", "Bypass", ".\{0}.ps1".format(script)]
+@app.route("/", methods=['GET'])
+def execute_script(script = 'test'):
+    cmd = ["powershell","-ExecutionPolicy", "Bypass", ".\{0}.ps1".format(script),""]
     print(cmd)
     p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     out,err = p.communicate()
