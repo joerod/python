f37b3bc14969885dae3e85ddab0b1e9372ba6e29
Joe Rodriguez <joerod@gmail.com>
2021-12-22 01:11:16 -0500

    Update download_torrent.py

--- download_torrent.py
+++ download_torrent.py
@@ -1,15 +1,16 @@
-# download your torrent magnets via the command line and qbittorrent
 import argparse
 import qbittorrentapi
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(
+    description='download your torrent magnets via the command line and qbittorrent',
+    epilog="python download_torrent.py --host '192.168.1.221' --password 'Password' --magnet 'magnet:?xt=' --save_location '/downloads/Movies'"
+)
 parser.add_argument("--host", required=True, help="IP of machine running qbittorrent")
 parser.add_argument("--port", default='8080', help="Port of machine running qbittorrent")
 parser.add_argument("--username", default='admin', help="user name to login")
 parser.add_argument("--password", required=True, help="password to login")
 parser.add_argument("--magnet", required=True, help="magnet url")
 parser.add_argument("--save_location", required=True, help="where to save the torrent file")
-epilog="python download_torrent.py --host '192.168.1.221' --password 'Password' --magnet 'magnet:?xt=' --save_location '/downloads/Movies'"
 args = parser.parse_args()
 
 # instantiate a Client using the appropriate WebUI configuration
