97538e20f652e2d926ff48d8b39ad8fd3cf144bd
Joe Rodriguez <joerod@gmail.com>
2021-12-29 12:50:53 -0500

    Update gmail.py

--- gmail.py
+++ gmail.py
@@ -1,36 +1,31 @@
 from __future__ import print_function
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 import sys
+import os.path
 import argparse
 
 def main(scopes,cred_dir,q):
-    """
-      Will use a gmail search query to find emails based on some criteria
-      and delete them in batches of 1000 which is a gmail limitation
-      Usage: python3 gmail.py --cred_dir '/home/joerod/Downloads' --q 'older_than:1y category:forums -is:starred' --scopes ['https://mail.google.com/']
-    """
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists(os.path.join(cred_dir, 'token.pickle')):
-        with open(os.path.join(cred_dir, 'token.pickle'), 'rb') as token:
-            creds = pickle.load(token)
+    if os.path.exists(os.path.join(cred_dir, 'token.json')):
+        creds = Credentials.from_authorized_user_file(os.path.join(cred_dir, 'token.json'), scopes)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                os.path.join(cred_dir, 'credentials.json'), scopes)
+                'credentials.json', scopes)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open(os.path.join(cred_dir, 'token.pickle'), 'wb') as token:
-            pickle.dump(creds, token)
+        if os.path.join(cred_dir, 'token.json'):
+            with open(os.path.join(cred_dir, 'token.json'), 'w') as token:
+                token.write(creds.to_json())
+        else:
+            print(f"Cound not fine {os.path.join(cred_dir, 'token.json')}")        
 
     service = build('gmail', 'v1', credentials=creds)
     for i in range(100):
@@ -72,30 +67,26 @@ def prep_messages_for_delete(raw_messages):
     return message
 
 
-def batch_delete_messages(service, messages):
+def batch_delete_messages(service, messages,userId='me'):
     print("ready to delete {} messages".format(len(messages['ids'])))
-    user_id = "me"
-
     try:
         service.users().messages().batchDelete(
-            userId=user_id,
+            userId=userId,
             body=messages
         ).execute()
-
         print("I deleted stuff!")
     except errors.HttpError as error:
         print('An error occurred while batchDeleting: {}'.format(error))
 
 if __name__ == '__main__':
+    """
+      Will use a gmail search query to find emails based on some criteria
+      and delete them in batches of 1000 which is a gmail limitation
+      Usage: python gmail.py --cred_dir '/home/joerod/Downloads' --query 'older_than:1y category:forums -is:starred' --scopes 'https://mail.google.com/'
+    """
     parser = argparse.ArgumentParser(description='Gmail deleter')
-    parser.add_argument('--scopes', metavar="['https://mail.google.com/']", required=True,
-                        help='gmail scope',
-                        const="https://mail.google.com/",
-                        nargs='?',
-                        type=str)
-    parser.add_argument('--cred_dir', metavar='path', required=True,
-                        help='directory of credentials')
-    parser.add_argument('--q', metavar='older_than:1y category:forums -is:starred', required=True,
-                        help='gmail query to find emails')
+    parser.add_argument('--scopes', required=True,help='gmail scope', nargs='+', default=[])
+    parser.add_argument('--cred_dir', default='.',help='directory of credentials.json file')
+    parser.add_argument('--query','--q', required=True, help='gmail query to find emails')
     args = parser.parse_args()
-    main(scopes=args.scopes, cred_dir=args.cred_dir, q=args.q)
+    main(scopes=args.scopes, cred_dir=args.cred_dir, q=args.query)
