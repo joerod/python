26e64e50aba8bb4a6a3189cb817cf89de9c579d3
Joe Rodriguez <joerod@gmail.com>
2022-02-03 23:20:05 -0500

    Update leech_me.py
    made refreshing plex optional

--- leech_me.py
+++ leech_me.py
@@ -11,9 +11,9 @@ parser.add_argument("--torrent_host", required=True, help="IP of machine running
 parser.add_argument("--torrent_port", default='8080', help="Port of machine running qbittorrent")
 parser.add_argument("--username", default='admin', help="user name to login")
 parser.add_argument("--password", required=True, help="password to login")
-parser.add_argument("--plex_host", required=True, help="IP of machine running Plex")
+parser.add_argument("--plex_host", help="IP of machine running Plex")
 parser.add_argument("--plex_port", default='32400', help="IP of machine running Plex")
-parser.add_argument("--plex_token", required=True, help="token to auth to Plex")
+parser.add_argument("--plex_token", help="token to auth to Plex")
 args = parser.parse_args()
 
 # instantiate a qbittorrent client using the appropriate WebUI configuration
@@ -24,11 +24,12 @@ try:
 except qbittorrentapi.LoginFailed as e:
     print(e)
 
-# instantiate a plex connection 
-try:
-    plex = PlexServer("http://" + args.plex_host +  ":" + args.plex_port, args.plex_token)    
-except Exception as e:
-    print(e)
+if(args.plex_host):
+    # instantiate a plex connection 
+    try:
+        plex = PlexServer("http://" + args.plex_host +  ":" + args.plex_port, args.plex_token)    
+    except Exception as e:
+        print(e)
 
 start = time.time()
 while(len(qbt_client.torrents_info())>0):
@@ -37,21 +38,20 @@ while(len(qbt_client.torrents_info())>0):
         if(torrents.state in ['uploading','stalledUP']):
             print(f'Removing {torrents.name}')
             qbt_client.torrents_delete(torrent_hashes=torrents.hash)
-            # remove unneeded path info from download
-            refresh_path = torrents.save_path.replace('/downloads/','').replace('/','')
-            plex.library.section(refresh_path).update()
-            print(f'Refreshed Plex path "{refresh_path}"')
-            if(len(qbt_client.torrents_info())==0):
-                  end = time.time()
-                  total_time = round((end-start)/60, 0)
-                  plural_time = 'minute' if total_time <= 1 else 'minutes'
-                  print(f'All leached up: leaching took {total_time} {plural_time}')
-                  quit()
+            if(args.plex_host):
+                # remove unneeded path info from download
+                refresh_path = torrents.save_path.replace('/downloads/','').replace('/','')
+                plex.library.section(refresh_path).update()
+                print(f'Refreshed Plex path "{refresh_path}"')
+                if(len(qbt_client.torrents_info())==0):
+                    end = time.time()
+                    total_time = round((end-start)/60, 0)
+                    plural_time = 'minute' if total_time <= 1 else 'minutes'
+                    print(f'All leached up: leaching took {total_time} {plural_time}')
+                    quit()
     time.sleep(60)
 else:
   end = time.time()
   total_time = round((end-start)/60, 0)
   plural_time = 'minute' if total_time <= 1 else 'minutes'
   print(f'All leached up: leaching took {total_time} {plural_time}')
-
-  
